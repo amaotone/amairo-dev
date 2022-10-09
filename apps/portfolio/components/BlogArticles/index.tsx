6d95a3912856e650e73f96877cbe6ddd31d8a7e4
amaotone <amane.suzu@gmail.com>
2022-10-09 10:55:49 +0900

    feat: add loading anime

--- apps/portfolio/components/BlogArticles/index.tsx
+++ apps/portfolio/components/BlogArticles/index.tsx
@@ -1,5 +1,5 @@
 import { ExternalLinkIcon } from "@chakra-ui/icons";
-import { Box, Link, List, ListItem } from "@chakra-ui/react";
+import { Box, Link, List, ListItem, Spinner } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import Parser from "rss-parser";
 import strftime from "strftime";
@@ -11,6 +11,7 @@ interface Post {
 }
 
 export const BlogArticles: React.FC = () => {
+  const [loading, setLoading] = useState(true);
   const [posts, setPosts] = useState<Post[]>([]);
 
   useEffect(() => {
@@ -29,35 +30,43 @@ export const BlogArticles: React.FC = () => {
     };
 
     const fetchPosts = async () => {
+      setLoading(true);
       const url = "https://amalog.hateblo.jp/rss";
       const feed = await parser.parseURL(url);
       setPosts(parseItems(feed.items));
+      setLoading(false);
     };
     fetchPosts();
   }, []);
 
   return (
     <>
-      {posts && (
-        <>
-          <List>
-            {posts.map(post => {
-              return (
-                <ListItem key={post.title}>
-                  {strftime("%Y-%m-%d ", post.pubDate)}
-                  <Link color="teal" href={post.link}>
-                    {post.title}
-                  </Link>
-                </ListItem>
-              );
-            })}
-          </List>
-          <Box textAlign="right">
-            <Link color="teal" href="https://amalog.hateblo.jp">
-              and more <ExternalLinkIcon mx="2px" />
-            </Link>
-          </Box>
-        </>
+      {loading ? (
+        <Box textAlign="center">
+          <Spinner size="xl" colorScheme="teal" />
+        </Box>
+      ) : (
+        posts && (
+          <>
+            <List>
+              {posts.map(post => {
+                return (
+                  <ListItem key={post.title}>
+                    {strftime("%Y-%m-%d ", post.pubDate)}
+                    <Link color="teal" href={post.link}>
+                      {post.title}
+                    </Link>
+                  </ListItem>
+                );
+              })}
+            </List>
+            <Box textAlign="right">
+              <Link color="teal" href="https://amalog.hateblo.jp">
+                and more <ExternalLinkIcon mx="2px" />
+              </Link>
+            </Box>
+          </>
+        )
       )}
     </>
   );

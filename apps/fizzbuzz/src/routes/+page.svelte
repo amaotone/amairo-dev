276d9fa9bec7625ca01bed8694c706db292df7c8
amaotone <amane.suzu@gmail.com>
2023-09-23 21:10:48 +0900

    feat: add fizzbuzz

---
+++ apps/fizzbuzz/src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+	import Counter from './Counter.svelte';
+</script>
+
+<svelte:head>
+	<title>FizzBuzz</title>
+	<meta name="description" content="fizzbuzz clicker" />
+</svelte:head>
+
+<section>
+	<Counter />
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+</style>
---
+++ apps/fizzbuzz/src/routes/Counter.svelte
@@ -0,0 +1,91 @@
+<script lang="ts">
+	let count = 0;
+
+	function fizzbuzz(n: number) {
+		if (n % 15 === 0) return 'FizzBuzz';
+		if (n % 3 === 0) return 'Fizz';
+		if (n % 5 === 0) return 'Buzz';
+		return n;
+	}
+</script>
+
+<div class="counter">
+	<div class="counter-viewport">
+		<div class="counter-digits">
+			{fizzbuzz(count)}
+		</div>
+	</div>
+
+	<button on:click={() => (count += 1)} aria-label="Increase the counter by one">
+		<svg aria-hidden="true" viewBox="0 0 1 1">
+			<path d="M0,0.5 L1,0.5 M0.5,0 L0.5,1" />
+		</svg>
+	</button>
+</div>
+
+<style>
+	.counter {
+		display: flex;
+		border-top: 1px solid rgba(0, 0, 0, 0.1);
+		border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+		margin: 1rem 0;
+	}
+
+	.counter button {
+		width: 2em;
+		padding: 0;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		border: 0;
+		background-color: transparent;
+		touch-action: manipulation;
+		font-size: 2rem;
+	}
+
+	.counter button:hover {
+		background-color: var(--color-bg-1);
+	}
+
+	svg {
+		width: 25%;
+		height: 25%;
+	}
+
+	path {
+		vector-effect: non-scaling-stroke;
+		stroke-width: 2px;
+		stroke: #444;
+	}
+
+	.counter-viewport {
+		width: 8em;
+		height: 4em;
+		overflow: hidden;
+		text-align: center;
+		position: relative;
+	}
+
+	.counter-viewport strong {
+		position: absolute;
+		display: flex;
+		width: 100%;
+		height: 100%;
+		font-weight: 400;
+		color: var(--color-theme-1);
+		font-size: 4rem;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.counter-digits {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+	}
+
+	.hidden {
+		top: -100%;
+		user-select: none;
+	}
+</style>

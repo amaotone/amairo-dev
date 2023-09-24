ce1dbea22a0c3c070afa74edef2b8455ddff922a
amaotone <amane.suzu@gmail.com>
2023-09-24 16:37:09 +0900

    feat: use svelte/store

--- apps/fizzbuzz/src/routes/+page.svelte
+++ apps/fizzbuzz/src/routes/+page.svelte
@@ -1,5 +1,24 @@
-<script>
-	import Counter from './Counter.svelte';
+<script lang="ts">
+	import { Button } from "$lib/components/ui/button";
+	import * as Card from '$lib/components/ui/card';
+	import * as Table from '$lib/components/ui/table';
+	import Counter from "./Counter.svelte";
+	import { score } from './scores';
+	import { upgrades } from './upgrades';
+
+	// function autoClick({upgrade}) {
+	// 	const delay = Math.max(1000/upgrade.cps, 1);
+	// 	const increaseAmount = upgrade.cps > 1000 ? upgrade.cps / 1000 : 1;
+
+	// }
+
+	const increment = () => score.update((n)=>n+1);
+
+	let clear: NodeJS.Timer;
+	$: {
+		clearInterval(clear);
+		clear = setInterval(increment, 1000);
+	}
 </script>
 
 <svelte:head>
@@ -7,16 +26,27 @@
 	<meta name="description" content="fizzbuzz clicker" />
 </svelte:head>
 
-<section>
+<div class="container mx-auto p-4">
 	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-</style>
+	<Button on:click={increment}>+1</Button>
+	<Card.Root>
+		<Table.Root>
+			<Table.Header>
+				<Table.Row class="font-light">
+					<Table.Cell>Name</Table.Cell>
+					<Table.Cell>Count</Table.Cell>
+					<Table.Cell  class="text-right">Cost(fb)</Table.Cell>
+				</Table.Row>
+			</Table.Header>
+			<Table.Body>
+			{#each upgrades as item}
+				<Table.Row>
+					<Table.Cell class="font-extrabold">{item.name} ({item.cps}/s)</Table.Cell>
+					<Table.Cell>0</Table.Cell>
+					<Table.Cell class="text-right"><Button class="">{Intl.NumberFormat("en-US", {notation: 'compact', maximumFractionDigits: 1}).format(item.cost)}</Button></Table.Cell>
+				</Table.Row>
+			{/each}
+			</Table.Body>
+		</Table.Root>
+	</Card.Root>
+</div>
--- apps/fizzbuzz/src/routes/Counter.svelte
+++ apps/fizzbuzz/src/routes/Counter.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
-	let count = 0;
+	import { score } from './scores';
+
+
+	let count=0;
+	score.subscribe(value => {
+		count = value
+	})
 
 	function fizzbuzz(n: number) {
+		if (n===0) return "Start";
 		if (n % 15 === 0) return 'FizzBuzz';
 		if (n % 3 === 0) return 'Fizz';
 		if (n % 5 === 0) return 'Buzz';
@@ -9,83 +16,5 @@
 	}
 </script>
 
-<div class="counter">
-	<div class="counter-viewport">
-		<div class="counter-digits">
-			{fizzbuzz(count)}
-		</div>
-	</div>
-
-	<button on:click={() => (count += 1)} aria-label="Increase the counter by one">
-		<svg aria-hidden="true" viewBox="0 0 1 1">
-			<path d="M0,0.5 L1,0.5 M0.5,0 L0.5,1" />
-		</svg>
-	</button>
-</div>
-
-<style>
-	.counter {
-		display: flex;
-		border-top: 1px solid rgba(0, 0, 0, 0.1);
-		border-bottom: 1px solid rgba(0, 0, 0, 0.1);
-		margin: 1rem 0;
-	}
-
-	.counter button {
-		width: 2em;
-		padding: 0;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		border: 0;
-		background-color: transparent;
-		touch-action: manipulation;
-		font-size: 2rem;
-	}
-
-	.counter button:hover {
-		background-color: var(--color-bg-1);
-	}
-
-	svg {
-		width: 25%;
-		height: 25%;
-	}
-
-	path {
-		vector-effect: non-scaling-stroke;
-		stroke-width: 2px;
-		stroke: #444;
-	}
-
-	.counter-viewport {
-		width: 8em;
-		height: 4em;
-		overflow: hidden;
-		text-align: center;
-		position: relative;
-	}
-
-	.counter-viewport strong {
-		position: absolute;
-		display: flex;
-		width: 100%;
-		height: 100%;
-		font-weight: 400;
-		color: var(--color-theme-1);
-		font-size: 4rem;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.counter-digits {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-	}
-
-	.hidden {
-		top: -100%;
-		user-select: none;
-	}
-</style>
+<p class="scroll-m-20 text-4xl font-bold tracking-tight lg:text-5xl">{fizzbuzz(count)}</p>
+<p class="scroll-m-20 text-4xl font-bold tracking-tight lg:text-5xl">{Math.floor(count / 15)} fb</p>

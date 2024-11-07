7ca1e26d3c99fe7679ec67555cfa7d616f431ae2
amaotone <amane.suzu@gmail.com>
2024-11-07 23:54:44 +0900

    chore(fizzbuzz): remove unused files and configurations to clean up the project structure and improve maintainability

--- apps/fizzbuzz/src/routes/scores.ts
+++
@@ -1,3 +0,0 @@
-import { writable } from 'svelte/store';
-
-export const scoreStore = writable(0);
--- apps/fizzbuzz/src/routes/utils.ts
+++
@@ -1,7 +0,0 @@
-export function formatNumber(n: number) {
-	return Intl.NumberFormat('en-US', { notation: 'compact', maximumFractionDigits: 1 }).format(n);
-}
-
-export function calcCost(baseCost: number, count: number) {
-	return Math.floor(baseCost * Math.pow(1.1, count));
-}
